b95dddd522b8c797d6bcd6e736e3fecdff6023d1
Wil Maguire <wil.maguire@gladstone.ucsf.edu>
2019-12-05 11:04:17 -0800

    comment out getUserPriv function

--- bananas_agw.pm
+++ bananas_agw.pm
@@ -1129,7 +1129,7 @@ sub get_qsub_cmd($;$) { # returns something like: qsub -q Bio -A "bioqueue" (wit
 	# or qsub -q General -A "genqueue" [/path/to/script]
 	#if (!defined($hr)) { my %h = (); $hr = \%h; }; # just define the hash ref if it wasn't actually passed in...
 	my $qsub         = getBinPath($cfg,"qsub");
-	my $userPriv     = getUserPriv();
+#	my $userPriv     = getUserPriv();
 	my $qdest_param  = $QSETTINGS{dest}{$userPriv};
 	my $qgroup_param = $QSETTINGS{grouplist}{$userPriv};
 	my $mem      = "-l mem_free="      . (defined($hr->{pbs_mem})      ? $hr->{pbs_mem}      : $QSETTINGS{default_mem}{$userPriv});
