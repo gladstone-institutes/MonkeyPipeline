1a44aa9aac17dfa7942739235f1a87c8475197ee
Wil Maguire <wil.maguire@gladstone.ucsf.edu>
2019-12-23 09:17:54 -0800

    closing parenths

--- bananas_agw.pm
+++ bananas_agw.pm
@@ -1906,7 +1906,7 @@ sub runJobs {	      # Generate AND RUN the qsub "list of jobs to submit" file.
 		my $WALLTIME_FOR_WAIT_JOBS = "300"; # these really only take like 1 second to run
 		my $IMMEDIATELY_HOLD_JOB   = " -h "; # Starts the job in the HELD state, until it is 'qrls'-ed.
 		# Note that this job gets HELD immediately upon submission! It can't run until it gets 'qrls'-ed later.
-		appendLinesInPlaceNL(\$lnum, \$OUT_HEADER_PRINT, qq{${SIGNAL_TO_START_VARNAME}=`echo "sleep 1" | } . get_qsub_cmd($cfg, {'pbs_walltime'=>$WALLTIME_FOR_WAIT_JOBS, 'pbs_stderr'=>"/dev/null", 'pbs_stdout'=>"/dev/null" }) . qq{ ${IMMEDIATELY_HOLD_JOB} -V -N $cfg->{studyName}_Start_signal}) . qq{| awk '{print \$3}'`};
+		appendLinesInPlaceNL(\$lnum, \$OUT_HEADER_PRINT, qq{${SIGNAL_TO_START_VARNAME}=`echo "sleep 1" | } . get_qsub_cmd($cfg, {'pbs_walltime'=>$WALLTIME_FOR_WAIT_JOBS, 'pbs_stderr'=>"/dev/null", 'pbs_stdout'=>"/dev/null" }) . qq{ ${IMMEDIATELY_HOLD_JOB} -V -N $cfg->{studyName}_Start_signal} . qq{| awk '{print \$3}'`});
 	}
 	print OF $OUT_HEADER_PRINT;
 	#printf OF ($GLOBAL_VERBOSE) ? "set -o verbose  # <== verbose \n" : "# (Not setting verbose mode) \n";
