91aae590c426c0a724d0e42bcf7cfc49d90ce557
Wil Maguire <wil.maguire@gladstone.ucsf.edu>
2019-12-23 16:16:00 -0800

    try jidname here

--- bananas_agw.pm
+++ bananas_agw.pm
@@ -1918,7 +1918,7 @@ sub runJobs {	      # Generate AND RUN the qsub "list of jobs to submit" file.
 			my $OUTPRINT = '';
 			($GLOBAL_VERBOSE) and appendLinesInPlaceNL(\$lnum, \$OUTPRINT, qq{echo ''});
 			if ($GLOBAL_VERBOSE && !$RUN_DIRECTLY_WITHOUT_TORQUE) {
-				appendLinesInPlaceNL(\$lnum, \$OUTPRINT, qq{echo '${jname} dependencies:' });
+				appendLinesInPlaceNL(\$lnum, \$OUTPRINT, qq{echo '${jidname} dependencies:' });
 				my @dependenciesArr = split(":", $remember{$REM_DEPENDENCIES_STR_COLON_DELIM}{$jidname});
 				foreach my $d (@dependenciesArr) {
 					$d =~ s/^[\$]//; # Remove the leading '$' from each variable so it doesn't get auto-evaluated when we $echo it
