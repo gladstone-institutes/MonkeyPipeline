b5cd36a898e8b14ee70ba4001ded34c841c7d469
Wil Maguire <wil.maguire@gladstone.ucsf.edu>
2019-12-23 16:22:14 -0800

    quotes

--- bananas_agw.pm
+++ bananas_agw.pm
@@ -1914,7 +1914,7 @@ sub runJobs {	      # Generate AND RUN the qsub "list of jobs to submit" file.
 		foreach my $sampleName (sort keys %{$cfg->{jobs}->{$stepName}} ) {
 			my $qcmd  = $cfg->{jobs}->{$stepName}->{$sampleName}->{qsub};
 			my $jname = $cfg->{jobs}->{$stepName}->{$sampleName}->{jobName};
-			my $jidname = `"echo "$jname" |awk '{print \$3}'"`;
+			my $jidname = "echo "$jname" |awk '{print \$3}'";
 			my $OUTPRINT = '';
 			($GLOBAL_VERBOSE) and appendLinesInPlaceNL(\$lnum, \$OUTPRINT, qq{echo ''});
 			if ($GLOBAL_VERBOSE && !$RUN_DIRECTLY_WITHOUT_TORQUE) {
